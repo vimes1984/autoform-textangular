ba130112db8aba3ecc332280c08f53d0431da9ef
vimes1984 <churchill.c.j@gmail.com>
2015-08-27 02:04:28 +0200

    newsest version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'vimes1984:autoform-textangular',
-  version: '1.1.1',
+  version: '1.1.2',
   // Brief, one-line summary of the package.
   summary: 'Integrate textangular into autoforms',
   // URL to the Git repository containing the source code for this package.
